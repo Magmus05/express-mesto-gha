3369d1b0e30fe91d30b6b4e3b8e4d2b80817dec6
Magomed Musakadiev <musakadiev-mag@yandex.ru>
2023-07-25 12:12:07 +0300

    первый коммит

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,27 @@
 /* eslint-disable */
 const User = require("../models/user");
-
+const ERROR_CODE_BAD_REQUEST = 400;
+const ERROR_CODE_NOT_FOUND = 404;
+const ERROR_CODE_INTERNAL_SERVER_ERROR = 500;
 function getUsers(req, res) {
   User.find({})
     .then((users) => {
       res.status(200);
-      res.send(users.map((user) => {
-        return {
-          name: user.name,
-          about: user.about,
-          avatar: user.avatar,
-          _id: user._id,
-        };
-      }));
+      res.send(
+        users.map((user) => {
+          return {
+            name: user.name,
+            about: user.about,
+            avatar: user.avatar,
+            _id: user._id,
+          };
+        })
+      );
     })
     .catch((err) => {
-      const ERROR_CODE = 500;
       if (err.name === "SomeErrorName")
-        return res.status(ERROR_CODE).send({ message: "Ошибка запроса" });
-      res.status(ERROR_CODE).send(err.message);
+        return res.status(ERROR_CODE_INTERNAL_SERVER_ERROR).send({ "message": "Ошибка запроса" });
+      res.status(ERROR_CODE_INTERNAL_SERVER_ERROR).send(err.message);
     });
 }
 
@@ -35,12 +38,11 @@ function getUser(req, res) {
       res.status(200).send(u);
     })
     .catch((err) => {
-      const ERROR_CODE = 404;
-      if (err.name === "SomeErrorName")
-        return res
-          .status(ERROR_CODE)
-          .send({ message: "Запрашиваемый пользователь не найден" });
-      res.status(ERROR_CODE).send(err.message);
+      if (err.name === "TypeError")
+        return res.status(ERROR_CODE_NOT_FOUND).send({ "message": "Такой ID не существует" });
+      if (err.name === "CastError")
+        return res.status(ERROR_CODE_BAD_REQUEST).send({ "message": "Не верный ID" });
+      res.status(ERROR_CODE_INTERNAL_SERVER_ERROR).send(err.message);
     });
 }
 
@@ -56,12 +58,9 @@ function createUser(req, res) {
       res.status(201).send(u);
     })
     .catch((err) => {
-      const ERROR_CODE = 400;
-      res
-        .status(ERROR_CODE)
-        .send({
-          message: "Переданы некорректные данные при создании пользователя",
-        });
+      res.status(ERROR_CODE_BAD_REQUEST).send({
+        "message": "Переданы некорректные данные при создании пользователя",
+      });
     });
 }
 
@@ -71,6 +70,8 @@ function updateUserProfile(req, res) {
     about: req.body.about,
   })
     .then((user) => {
+      if(req.body.name.length<2 || req.body.name.length>30 || req.body.about.length<2 || req.body.about.length>30 ){return Promise.reject(res.status)}
+      console.log(req.body.name.length);
       const u = {
         name: req.body.name,
         about: req.body.about,
@@ -80,12 +81,14 @@ function updateUserProfile(req, res) {
       res.status(200).send(u);
     })
     .catch((err) => {
-      const ERROR_CODE = 400;
-      res
-        .status(ERROR_CODE)
-        .send({
-          message: "Переданы некорректные данные при обновлении пользователя",
-        });
+      // console.log(err);
+      // console.log(err.name);
+      // console.log(err.code);
+      // console.log(err.kind);
+      // console.log(req.body.name.length);
+      res.status(ERROR_CODE_BAD_REQUEST).send({
+        "message": "Переданы некорректные данные при обновлении пользователя",
+      });
     });
 }
 function updateUserAvatar(req, res) {
@@ -100,12 +103,9 @@ function updateUserAvatar(req, res) {
       res.status(200).send(u);
     })
     .catch((err) => {
-      const ERROR_CODE = 400;
-      res
-        .status(ERROR_CODE)
-        .send({
-          message: "Переданы некорректные данные при обновлении аватара.",
-        });
+      res.status(ERROR_CODE_BAD_REQUEST).send({
+        "message": "Переданы некорректные данные при обновлении аватара.",
+      });
     });
 }
 
